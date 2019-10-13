d612a517e12414b4d96a1a462af2d0ed81f8c65f
Louis345 <jt125845@gmail.com>
2019-10-13 16:29:56 -0400

    Added challenges for 10-14

---
+++ challenges/10-14-challenges/10-14-challenges.js
@@ -0,0 +1,38 @@
+/**
+ * Using the spread operator remove the first  element from the array
+ *
+ * @param {array} arr
+ * @returns {mixed} an array of values with the values with one of the values removed
+ *
+ * @example removeElement(['one','two','three']) // ['two', 'three']
+ */
+
+function removeElement(arr) {}
+
+/**
+ * truncate a string
+ * Truncate a string (first argument) if it is longer than the given maximum string length (second argument).
+ * Return the truncated string with a ...ending
+ *
+ * @param {string} string
+ * @returns Return the truncated string with a ... as the ending
+ *
+ * @example
+ *
+ *
+ * truncateString("A-tisket a-tasket A green and yellow basket", 8)
+ * should return "A-tisket...".
+ *
+ *  truncateString("A-", 1)should return "A...".
+ *
+ * truncateString("Peter Piper picked a peck of pickled peppers", 11)
+ *  should return "Peter Piper..."
+ *
+ */
+
+function truncateString(str, num) {}
+
+module.exports = {
+  removeElement,
+  truncateString
+};
