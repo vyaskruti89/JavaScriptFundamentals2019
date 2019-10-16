99f930b5a78080d0e69a211a35f4e922876f2c4a
Louis345 <jt125845@gmail.com>
2019-10-16 19:17:59 -0400

    Added ForEach execrises

--- exercises/06-A-Closer-Look-At-Functions/06-A-Closer-Look-At-Functions.js
+++ exercises/06-A-Closer-Look-At-Functions/06-A-Closer-Look-At-Functions.js
@@ -147,6 +147,26 @@ const printNames = () => {};
 
 /*** Higher Order Functions Starts */
 
+/**** ForEach */
+
+/**
+ * Build the forEach function yourself
+ * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/forEach
+ * @param {array} arr
+ * @param {function} callback
+ */
+const forEach = (arr, callback) => {};
+
+/**
+ * Given an array of strings, remove all letters of each value except the first and last character in the strings
+ * @param {array} arr
+ * @returns {array} new array, where each values is the first and character of each string
+ * @example
+ *   showFirstAndLast(['colt','matt', 'tim', 'udemy']); // ["ct", "mt", "tm", "uy"]
+ *   showFirstAndLast(['hi', 'goodbye', 'smile']) // ['hi', 'ge', 'se']
+ */
+const showFirstAndLast = arr => {};
+
 /**
  * Recreate the map function
  * @see https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/map
@@ -300,6 +320,8 @@ module.exports = {
   printer,
   printNames,
   multiplier,
+  forEach,
+  showFirstAndLast,
   map,
   doubleValues,
   filter,
--- test/06-A-Closer-Look-At-Functions/06-A-Closer-Look-At-Functions-test.js
+++ test/06-A-Closer-Look-At-Functions/06-A-Closer-Look-At-Functions-test.js
@@ -8,6 +8,8 @@ const {
   printNames,
   multiplier,
   map,
+  forEach,
+  showFirstAndLast,
   doubleValues,
   filter,
   deleteUser,
@@ -152,6 +154,26 @@ describe("Callbacks", () => {
     const callback = sinon.spy();
     printNames(["test", "test1", "test3"], callback);
     expect(callback.calledThrice).to.equal(true);
+  });
+});
+
+describe("forEach", () => {
+  it("tests forEach", () => {
+    let numbersDoubled = [];
+    let suppliedArr = [];
+    let arrIndex = [];
+    forEach([1, 2, 3], (number, idx, arr) => {
+      arrIndex.push(idx);
+      numbersDoubled.push(number * 2);
+      suppliedArr = arr;
+    });
+    expect(arrIndex).to.be.equalTo([0, 1, 2]);
+    expect(suppliedArr).to.be.equalTo([1, 2, 3]);
+    expect(numbersDoubled).to.be.equalTo([2, 4, 6]);
+  });
+  it("showFirstAndLast should print only the first and last letter of a string", () => {
+    const abbreviatedName = showFirstAndLast(["colt", "matt", "tim", "udemy"]);
+    expect(abbreviatedName).to.be.equalTo(["ct", "mt", "tm", "uy"]);
   });
 });
 
