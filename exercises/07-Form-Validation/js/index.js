299f6632babc5c94d45d0ddbe5bf25eec776bc5f
Louis345 <jt125845@gmail.com>
2019-10-28 17:45:08 -0400

    Challenge for Form Validation

---
+++ exercises/07-Form-Validation/js/index.js
@@ -0,0 +1,20 @@
+/**
+ * As a engineer, I would like to have my form validated so the information is safe and correct.
+*  
+*  All the fields should be required!!
+
+*  If a field is left empty on form submission, 
+*  the form will display a red box indicating to the user that there has been an error.
+*  
+*  If the form is valid, the form should submit, and the user should see a confirmation of information they filled out. (Developers Discretion)
+
+
+* Bonus Points
+*  1) In addition to adding a class when a field is not valid display a message requesting the user to enter their information. 
+*  2) Create a button that resets the form. 
+
+
+* Developer notes
+*  1) An error class named .error you can use to apply the proper css to an invalid element.
+*  2) You can edit this form however you see fit as the engineer to achieve your goals. (i.e add ids or additional classes if needed)
+*/
