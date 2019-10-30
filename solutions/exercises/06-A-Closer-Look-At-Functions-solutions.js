fd73935474b7681ae03f9ea8e03e8f33d71d79e1
Matina Patsos <matinaspatsos@gmail.com>
2019-10-29 21:25:30 -0400

    Reduce solutions

--- solutions/exercises/06-A-Closer-Look-At-Functions-solutions.js
+++ solutions/exercises/06-A-Closer-Look-At-Functions-solutions.js
@@ -346,3 +346,68 @@ const addItems = arr => {
 };
 // Solution 2
 const addItems = arr => arr.reduce((sum, number) => sum + number);
+
+/**
+ * Create a function that flattens an array (that is, it should "unnest" a nested array).
+ * @param {array} array e.g. `[[1, 3], [5, 10]]`
+ * @returns {array} new, flattened array e.g. `[1, 3, 5, 10]`
+ */
+
+// Solution 1
+const flattenArray = array => {
+  return array.reduce((flattenedArray, currArray) => {
+    flattenedArray = flattenedArray.concat(currArray);
+    return flattenedArray;
+  }, []);
+};
+// Solution 2
+const flattenArray = array =>
+  array.reduce((flattenedArray, currArray) => [
+    ...flattenedArray,
+    ...currArray
+  ]);
+
+/**
+ * Create a function that tallies the number of each kind of "thing" within the array
+ * @param {array} array e.g. `['Apple', 'Orange', 'Apple', 'Blueberry']`
+ * @returns {object} where the thing name is the key and the tally is the value
+ * @example
+ *   let fruits = ['Apple', 'Orange', 'Apple', 'Blueberry', 'Grape', 'Grape'];
+ *   generateTally(generateTally); // {Apple: 2, Orange: 1, Blueberry: 1, Grape: 2}
+ */
+const generateTally = array => {
+  return array.reduce((tally, item) => {
+    if (tally[item]) tally[item] += 1;
+    else tally[item] = 1;
+    return tally;
+  }, {});
+};
+
+/**
+ * Create a function, that when given an array of object literals, will index the object literals by a single column
+ * (in this case, the ID). The function should return an object where the ID is the key and value is the the
+ * object literal.
+ *
+ * @param {array} arr an array of nested objects, where each object as a key called "ID"
+ * @returns {object} an object where the key is the ID and the value is the entire nested object
+ * @example
+ *
+ * let people = [
+ *  {id: 123, name: 'Dave', age: 23},
+ *  {id, 456, name: 'Rachel', age: 35}
+ * ];
+ *
+ * let result =  arrayToObject();
+ *
+ * // result should be:
+ * {
+ *   123: {id: 123, name: 'Dave', age: 23},
+ *   456: {id, 456, name: 'Rachel', age: 35}
+ * }
+ */
+const arrayToObject = arr => {
+  return arr.reduce((obj, person) => {
+    obj[person.id] = person;
+    return obj;
+  }, {});
+};
