360de2a904ad4c21cae5c8911786dd175c3f4469
Matina Patsos <matinaspatsos@gmail.com>
2019-11-09 13:54:46 -0500

    DOM challenges

---
+++ challenges/11-11-challenges/11-11-challenges.js
@@ -0,0 +1,38 @@
+/**
+ * You have three challenges to solve below with Vanilla JavaScript.
+ * You are allowed to make changes to the HTML and CSS.
+ */
+
+/**
+ * Challenge 1: Show comments for the news story.
+ *
+ * When the user clicks on the "View Comments" button, the comment section should appear.
+ * Right now, the comments are hidden because they have the class ".hidden".
+ *
+ * BONUS: Clicking on the button should toggle instead of just show the comments.
+ * If the comments are open, change the button text from "View Comments" to "Hide Comments".
+ */
+
+/**
+ * Challenge 2: Display the results of the world's most pointless search engine.
+ *
+ * When the user types in the textbox  and clicks
+ * "Search", display the message "No results for ___ found" inside of this <p></p> below.
+ * For example, if the user searches for "Indian Ocean", display "No results for Indian Ocean found".
+ * (Since there are no oceans near Albany, NY, the search engine should
+ * display the "No results for ___ found" message every time.)
+ *
+ * The exercise must be completed with a form handler
+ * and you must prevent the page from refreshing when the form is submitted.
+ */
+
+/**
+ * Challenge 3: Adding pagination to the student table.
+ *
+ * First, when the page initially loads, only the first 10 students should be visible.
+ * (That is, every student row that has data-group="1"). You can just use CSS / HTML to do this.
+ *
+ * Second, clicking on the pagination below should show and hide different students in the table:
+ * - Clicking on the "«" and "1" buttons should show everything in data-group="1" and hide everything in data-group="2".
+ * - Clicking on the "2" and "»" buttons should show everything in data-group="2" and hide everything in data-group="1".
+ */
