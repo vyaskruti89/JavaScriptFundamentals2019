6537bcdc4174af1c4b92196b0c4945f62d2f851d
Louis345 <jt125845@gmail.com>
2019-11-20 17:29:06 -0500

    added api-key

--- exercises/WeatherApp/weather.js
+++ exercises/WeatherApp/weather.js
@@ -2,6 +2,7 @@
 
 /** Developer notes:
  *    Weather API: https://openweathermap.org/api
+ *     Api key: 886705b4c1182eb1c69f28eb8c520e20
  *
  *  1) Retrieve the user's latitude and longitude by using this API: https://ipapi.co/json/
  *  2) Using the user's latitude and longitude return the weather using this API: api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}
