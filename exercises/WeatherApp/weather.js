45366ce11b4db00c38d744feea67de13d69e3c2b
Louis345 <jt125845@gmail.com>
2019-11-19 22:06:50 -0500

    added weather challenge

---
+++ exercises/WeatherApp/weather.js
@@ -0,0 +1,12 @@
+/*** As a user, I would like to be able to see my weather forecast for the day */
+
+/** Developer notes:
+ *
+ *  1) Retrieve the user's latitude and longitude by using this API: https://ipapi.co/json/
+ *  2) Using the user's latitude and longitude return the weather using this API: api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}
+ *   a sample call will look like this: http://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=886705b4c1182eb1c69f28eb8c520e20
+ *  3) You can edit this weather app  however you see fit to achieve your goals. (i.e add ids or additional classes if needed). Make the weather app look more whimsical (ie icons based off of the weather forecast).
+ *
+ *
+ *
+ */
