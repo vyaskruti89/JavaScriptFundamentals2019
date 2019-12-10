098f88ab33d9969771d95371397b5e8b712fa99a
Matina Patsos <matinaspatsos@gmail.com>
2019-12-09 20:26:38 -0500

    More test corrections

--- test/12-Module-Pattern/Module-Pattern-test.js
+++ test/12-Module-Pattern/Module-Pattern-test.js
@@ -9,19 +9,12 @@ const {
 describe("Module Pattern", () => {
   it("should return a list of passing students", () => {
     const passingStudents = TeacherGradeBook.getPassingStudents();
-
-    expect(passingStudents).to.be.equalTo([
-      { average: 98 },
-      { average: 75 },
-      { average: 75 },
-      { average: 85 }
-    ]);
+    const students = passingStudents.map(student => student.firstName);
+    expect(students).to.be.equalTo(["John", "Bill", "Joe", "Billy"]);
   });
   it("should return a failing list of students", () => {
     const failingStudents = TeacherGradeBook.getFailingStudents();
-
-    expect(failingStudents).to.be.equalTo({
-      failingStudents: [{ average: 38 }, { average: 30 }]
-    });
+    const students = failingStudents.map(student => student.firstName);
+    expect(students).to.be.equalTo(["Jane", "Michael"]);
   });
 });
